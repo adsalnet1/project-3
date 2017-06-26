1a7d2952cbc8184e57cd3983f893531c91adbd05
Mr.Mostafa <Mr.Mostafa@DESKTOP-ULAKQB3>
2017-06-26 16:13:29 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            bool found = false;
+
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i;
+            }
+
+            Console.WriteLine("Enter a number: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+
+            foreach (var x in nums)
+            {
+                if (val == x)
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                Console.WriteLine("Value is found.");
+            }
+            else
+            {
+                Console.WriteLine("Value is not found.");
+            }
+        }
+    }
+}
